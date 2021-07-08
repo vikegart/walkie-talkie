9193e164f42cbf69413e3f4202eb8db85e9584bd
Vikentii Ekgart <vikegart@gmail.com>
2021-07-08 15:59:06 +0400

    new origin politics

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ const http = require("http").Server(app);
 const path = require('path');
 const cors = require('cors');
 
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
 const port = process.env.PORT || 3000;
 
 app.use(cors());
@@ -12,29 +16,29 @@ let Usercounter = 0;
 
 const voicesArr = [];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
-app.get("/voices", function(req, res) {
+app.get("/voices", function (req, res) {
   res.send(voicesArr);
 });
-app.get('/index.js', function(req, res) {
+app.get('/index.js', function (req, res) {
   res.sendFile((path.join(__dirname, '../client/index.js')));
 });
 
-io.on("connection", function(socket) {
+io.on("connection", function (socket) {
   Usercounter = Usercounter + 1;
   io.emit("user", Usercounter);
   console.log("a user is connected");
-  socket.on("disconnect", function() {
+  socket.on("disconnect", function () {
     Usercounter = Usercounter - 1;
     io.emit("user", Usercounter);
     console.log("user disconnected");
   });
 
-  socket.on("audioMessage", function(msg) {
+  socket.on("audioMessage", function (msg) {
     io.emit("audioMessage", msg);
-    voicesArr.push({timeStamp: Date(), audioBlob: msg});
+    voicesArr.push({ timeStamp: Date(), audioBlob: msg });
   });
 
   socket.on("recordStarted", () => {
@@ -42,6 +46,6 @@ io.on("connection", function(socket) {
   })
 });
 
-http.listen(port, function() {
+http.listen(port, function () {
   console.log("listening to port:" + port);
 });
